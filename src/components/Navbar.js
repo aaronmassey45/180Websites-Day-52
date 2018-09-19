571eddfac10e5c7dd35708d01702d9afa8404ee9
Aaron <aaronmassey45@gmail.com>
2018-09-19 07:35:22 -0500

    Updated Navbar  and moved into components folder

---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
+      <span className="navbar-brand h1 mb-0">Hex &lt;--&gt; RGB Converter</span>
+
+      <div className="navbar-nav mr-auto">
+        <a className="nav-item nav-link" href="https://aaronmassey.pro/">
+          Portfolio
+        </a>
+        <a
+          className="nav-item nav-link"
+          href="https://github.com/aaronmassey45/HexRGB-Converter"
+        >
+          Github
+        </a>
+      </div>
+    </nav>
+  );
+};
